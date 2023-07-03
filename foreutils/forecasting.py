8e29005445f4995b50980d40aed022ef895bc895
Tomás Couso <tcouso@uc.cl>
2023-07-02 21:01:43 -0400

    Fix: Forecaster used external model error.

--- foreutils/forecasting.py
+++ foreutils/forecasting.py
@@ -22,20 +22,18 @@ class Forecaster:
         if not self.model.fitted:
             self.model = self.model.fit(self.X, self.y, warmup=warmup)
 
-    def forecast(
-        self, model: reservoirpy.model.Model, T: int, memory: int = 52
-    ) -> np.ndarray:
+    def forecast(self, T: int, memory: int = 52) -> np.ndarray:
 
         ypred = np.empty((T, 1))
 
         # Reset internal state and feed the last 52 steps of time series
-        warmup_y = model.run(self.X[-memory:], reset=True)
+        warmup_y = self.model.run(self.X[-memory:], reset=True)
 
         # Generate first prediction
         x = np.concatenate((self.y[-(memory - 1) :].flatten(), warmup_y[-1]))
 
         for i in range(T):
-            prediction = model.run(x)
+            prediction = self.model.run(x)
             x = np.concatenate((x[-(memory - 1) :], prediction.flatten()))
             ypred[i] = prediction
 
