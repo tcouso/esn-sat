488c3228b2abcdff7c92ca4fd9eda6775cd3969a
Tomás Couso <tcouso@uc.cl>
2023-07-03 18:37:35 -0400

    Feat: Dataset creation function implemented.

--- foreutils/utils.py
+++ foreutils/utils.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from sklearn.model_selection import train_test_split
 
 def create_training_data(ts_data: np.ndarray, num_features: int) -> tuple[np.ndarray]:
     """
@@ -32,3 +32,51 @@ def create_training_data(ts_data: np.ndarray, num_features: int) -> tuple[np.nda
     y = ts_data[num_features:]
 
     return X, y
+
+
+def create_multivariate_training_data(ts_data: np.ndarray, num_features: int, train_size: float) -> tuple[np.ndarray]:
+    """
+    Create training and testing datasets from multiple time series.
+
+    Args:
+        ts_data (np.ndarray): 2D array representing multiple time series data.
+        num_features (int): Number of features (time steps) to use for each sample.
+        train_size (float): Proportion of the data to allocate for training.
+
+    Returns:
+        tuple[np.ndarray]: A tuple containing Xtrain, ytrain, Xtest, and ytest.
+
+    Raises:
+        None.
+
+    Example:
+        ts_data = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
+        num_features = 3
+        train_size = 0.5
+        Xtrain, ytrain, Xtest, ytest = create_multivariate_training_data(ts_data, num_features, train_size)
+    """
+    num_time_series = ts_data.shape[0]
+    all_Xtrain = []
+    all_ytrain = []
+    all_Xtest = []
+    all_ytest = []
+
+    # Create training and testing sets for each time series
+    for ts_idx in range(num_time_series):
+        ts = ts_data[ts_idx]
+        X, y = create_training_data(ts, num_features)
+        Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, train_size=train_size, shuffle=False)
+
+        all_Xtrain.append(Xtrain)
+        all_ytrain.append(ytrain)
+
+        all_Xtest.append(Xtest)
+        all_ytest.append(ytest)
+
+    # Concatenate the training and testing sets
+    Xtrain = np.concatenate(all_Xtrain)
+    ytrain = np.concatenate(all_ytrain)
+    Xtest = np.concatenate(all_Xtest)
+    ytest = np.concatenate(all_ytest)
+
+    return Xtrain, ytrain, Xtest, ytest
